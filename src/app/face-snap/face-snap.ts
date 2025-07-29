858461c4d0b43fb783169f078558b2b1767c385e
MrLkuch <kucheida.lucas@gmail.com>
2025-07-29 12:13:12 +0200

    feat(face-snap component): change button function to add and remove "like"

--- src/app/face-snap/face-snap.ts
+++ src/app/face-snap/face-snap.ts
@@ -13,15 +13,30 @@ export class FaceSnap {
   snaps!: number;
   imageUrl!: string;
 
+  snapButtonText!: string;
+  userHasSnapped!: boolean;
+
   ngOnInit(): void {
     this.title = 'Archibald';
     this.description = 'Mon meilleur ami depuis toujours !';
     this.createdAt = new Date();
     this.snaps = 5;
     this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
   }
 
-  onAddSnap() {
-  this.snaps++;
+  onSnap(): void {
+    if (this.userHasSnapped) {
+      this.snaps--;
+      this.snapButtonText = 'Oh Snap!';
+      this.userHasSnapped = false;
+    } else {
+      this.snaps++;
+      this.snapButtonText = 'Oops, unSnap!';
+      this.userHasSnapped = true;
+    }
+  }
 }
-}
+
