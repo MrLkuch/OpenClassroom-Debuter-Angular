4a06c6599be058c242625939d5fd5d549dbefddc
MrLkuch <kucheida.lucas@gmail.com>
2025-07-30 12:11:45 +0200

    feat(app component): use array of face-snap and display them

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,16 +10,32 @@ import { FaceSnapClass } from './models/face-snap-class';
 export class App {
   protected readonly title = signal('snapface');
 
-   mySnap!: FaceSnapClass;
+  faceSnaps!: FaceSnapClass[];
 
   ngOnInit() {
-    this.mySnap = new FaceSnapClass(
-      'Archibald',
-      'Mon meilleur ami depuis tout petit !',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      0
-    );
-    this.mySnap.setLocation('à la montagne');
+     this.faceSnaps = [
+      new FaceSnapClass(
+        'Archibald',
+        'Mon meilleur ami depuis toujours !',
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        new Date(),
+        10
+      ),
+      new FaceSnapClass(
+        'Three Rock Mountain',
+        'Un endroit magnifique pour les randonnées.',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        new Date(),
+        6
+      ),
+      new FaceSnapClass(
+        'Un bon repas',
+        'Mmmh que c\'est bon !',
+        'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        new Date(),
+        156
+      )
+    ];
+    this.faceSnaps[1].setLocation('à la montagne');
   }
 }
