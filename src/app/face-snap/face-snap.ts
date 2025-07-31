50d88c7d396dc09733647b069d40ad7614a8ce93
MrLkuch <kucheida.lucas@gmail.com>
2025-07-31 13:50:28 +0200

    feat(): Add single-face snap component to show one face snap

--- src/app/face-snap/face-snap.ts
+++ src/app/face-snap/face-snap.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import {FaceSnapClass } from '../models/face-snap-class';
 import { NgStyle, NgClass, UpperCasePipe, DatePipe } from '@angular/common';
-import { FaceSnapsService } from '../services/face-snaps.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-face-snap',
@@ -12,34 +12,10 @@ import { FaceSnapsService } from '../services/face-snaps.service';
 export class FaceSnap {
 
   @Input() FaceSnapClass!: FaceSnapClass;
-  constructor(private faceSnapsService: FaceSnapsService) {}
-
-  snapButtonText!: string;
-  userHasSnapped!: boolean;
-
-  ngOnInit(): void {
-    this.snapButtonText = 'Oh Snap!';
-    this.userHasSnapped = false;
-  }
-
-  onSnap(): void {
-    if(this.userHasSnapped){
-      this.unSnap();
-    } else {
-      this.snap();
+  
+  constructor(private router: Router) {}
+  onViewFaceSnap() {
+      this.router.navigateByUrl(`facesnaps/${this.FaceSnapClass.id}`);
     }
-  }
-
-  unSnap() {
-    this.faceSnapsService.snapFaceSnapById(this.FaceSnapClass.id, 'unsnap');
-    this.snapButtonText = 'Oh Snap!';
-    this.userHasSnapped = false;
-  }
-
-  snap() {
-    this.faceSnapsService.snapFaceSnapById(this.FaceSnapClass.id, 'snap');
-    this.snapButtonText = 'Oops, unSnap!';
-    this.userHasSnapped = true;
-  }
 }
 
