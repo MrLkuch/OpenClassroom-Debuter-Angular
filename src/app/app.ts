f3ca3eaa0445f864a37342ad964fd2e96d0d2a73
MrLkuch <kucheida.lucas@gmail.com>
2025-07-31 10:42:21 +0200

    feat(): Add simple header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { FaceSnap } from './face-snap/face-snap';
 import { FaceSnapList } from './face-snap-list/face-snap-list';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [FaceSnap, FaceSnapList],
+  imports: [FaceSnap, FaceSnapList,Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
