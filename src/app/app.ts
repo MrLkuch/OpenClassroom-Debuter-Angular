b20fc44e2ebe721487dff449d89f365775aedaf4
MrLkuch <kucheida.lucas@gmail.com>
2025-08-04 11:20:17 +0200

    feat(): Create high level operators

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
 import { RouterOutlet } from '@angular/router';
-import { interval, Observable, map, filter,tap } from 'rxjs';
+import { interval, of, concatMap, mergeMap, delay, exhaustMap, map, switchMap, take, tap } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
@@ -11,23 +11,31 @@ import { AsyncPipe } from '@angular/common';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('snapface');
-  interval$!: Observable<string>;
-  
- ngOnInit() {
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
 
-    this.interval$ = interval(1000).pipe(
-        filter(value => value % 3 === 0),
-        map(value => value % 2 === 0 ?
-            `Je suis ${value} et je suis pair` :
-            `Je suis ${value} et je suis impair`
-        ),
-        tap(text => this.logger(text))
+  ngOnInit() {
+    interval(500).pipe(
+      take(10),
+      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+      mergeMap(color => this.getTrainObservable$(color)),
+      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    ).subscribe();
+  }
+
+  getTrainObservable$(color: 'rouge' | 'jaune') {
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    return of({ color, trainIndex }).pipe(
+      delay(isRedTrain ? 5000 : 6000)
     );
   }
 
-  logger(text: string): void {
-    console.log(`Log: ${text}`);
-}
+  translateColor(color: 'rouge' | 'jaune') {
+    return color === 'rouge' ? 'red' : 'yellow';
+  }
  
 }
