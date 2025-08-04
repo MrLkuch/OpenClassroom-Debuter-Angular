3b92e00fd19e489054bdae258156a895a71dea05
MrLkuch <kucheida.lucas@gmail.com>
2025-08-04 12:51:21 +0200

    feat(): Create observable and unsubscribe function

--- src/app/face-snap-list/face-snap-list.ts
+++ src/app/face-snap-list/face-snap-list.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FaceSnap } from '../face-snap/face-snap';
 import { FaceSnapClass } from '../models/face-snap-class';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import { interval, tap, takeUntil, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -11,8 +12,23 @@ import { FaceSnapsService } from '../services/face-snaps.service';
 })
 export class FaceSnapList {
  faceSnaps!: FaceSnapClass[];
+  private destroy$!: Subject<boolean>;
+
  constructor(private faceSnapsService: FaceSnapsService) { }
  ngOnInit(): void {
+
+  this.destroy$ = new Subject<boolean>();
+
     this.faceSnaps = this.faceSnapsService.getFaceSnaps();
+    interval(1000).pipe(
+    tap(console.log),
+    takeUntil(this.destroy$)
+).subscribe();
+
+interval(1000).subscribe(value => console.log(value % 3 !== 0 ? 'Tick' : 'BANG'));
+}
+
+ngOnDestroy(): void {
+    this.destroy$.next(true);
 }
 }
